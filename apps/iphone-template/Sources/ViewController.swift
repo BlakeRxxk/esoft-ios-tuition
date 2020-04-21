1dc0b3171705e8470b5577247bce4d08c2097296
skyworkerxsky <pwuser@yandex.ru>
2020-04-21 16:55:52 +0500

    #10 вынес в компоненты все, поправил контроллер

--- apps/iphone-template/Sources/ViewController.swift
+++ apps/iphone-template/Sources/ViewController.swift
@@ -13,264 +13,90 @@ class ViewController: UIViewController {
   
   // MARK: - UI
   
-  private(set) lazy var container: UIView = {
-    let view = UIView()
-    view.backgroundColor = AppTheme.current().colors.container
-    view.translatesAutoresizingMaskIntoConstraints = false
-    return view
-  }()
+  private(set) lazy var container = UIView()
+  private(set) lazy var previewItemView = PreviewItemView()
+  private(set) lazy var addressItemView = AddressItemView()
+  private(set) lazy var infoItemView = InfoItemView()
   
-  // topView
+  private var activeConstraints: [NSLayoutConstraint] = []
   
-  private(set) lazy var topView: UIView = {
-    let view = UIView()
-    view.translatesAutoresizingMaskIntoConstraints = false
-    return view
-  }()
-  
-  private(set) lazy var topViewStack: UIStackView = {
-    let view = UIStackView()
-    view.translatesAutoresizingMaskIntoConstraints = false
-    return view
-  }()
-  
-  private(set) lazy var currentPrice: UILabel = {
-    let label = UILabel()
-    label.text = Localized.currentPrice
-    label.translatesAutoresizingMaskIntoConstraints = false
-    return label
-  }()
-  
-  private(set) lazy var priceStack: UIStackView = {
-    let stack = UIStackView()
-    stack.translatesAutoresizingMaskIntoConstraints = false
-    return stack
-  }()
-  
-  private(set) lazy var priceImage: UIImageView = {
-    let imageView = UIImageView()
-    imageView.image = UIImage.arrowDown
-    imageView.tintColor = AppTheme.current().colors.primary500
-    imageView.translatesAutoresizingMaskIntoConstraints = false
-    return imageView
-  }()
-  
-  private(set) lazy var price: UILabel = {
-    let label = UILabel()
-    label.text = Localized.price
-    label.translatesAutoresizingMaskIntoConstraints = false
-    return label
-  }()
-  
-  private(set) lazy var address: UILabel = {
-    let label = UILabel()
-    label.text = Localized.address
-    label.translatesAutoresizingMaskIntoConstraints = false
-    return label
-  }()
-  
-  // mainView
-  
-  private(set) lazy var mainView: UIView = {
-    let view = UIView()
-    view.translatesAutoresizingMaskIntoConstraints = false
-    return view
-  }()
-  
-  private(set) lazy var image: UIImageView = {
-    let imageView = UIImageView()
-    imageView.image = UIImage(named: "photo")
-    imageView.translatesAutoresizingMaskIntoConstraints = false
-    return imageView
-  }()
-  
-  private(set) lazy var favoriteButton: UIButton = {
-    let btn = UIButton()
-    btn.setBackgroundImage(UIImage.favorit, for: .normal)
-    btn.tintColor = .white
-    btn.translatesAutoresizingMaskIntoConstraints = false
-    return btn
-  }()
-  
-  // bottomView
-  
-  private(set) lazy var bottomView: UIView = {
-    let view = UIView()
-    view.translatesAutoresizingMaskIntoConstraints = false
-    return view
-  }()
-  
-  private(set) lazy var titleStack: UIStackView = {
-    let view = UIStackView()
-    view.translatesAutoresizingMaskIntoConstraints = false
-    return view
-  }()
-  
-  private(set) lazy var mainTitle: UILabel = {
-    let label = UILabel()
-    label.numberOfLines = 0
-    label.text = Localized.mainTitle
-    label.translatesAutoresizingMaskIntoConstraints = false
-    return label
-  }()
-  
-  private(set) lazy var phoneButton: UIButton = {
-    let btn = UIButton()
-    btn.setBackgroundImage(UIImage.Call.right, for: .normal)
-    btn.tintColor = AppTheme.current().colors.primary500
-    btn.translatesAutoresizingMaskIntoConstraints = false
-    return btn
-  }()
-  
-  // INFO STACK
-  private(set) lazy var infoStack: InfoItemView = {
-    let info = InfoItemView()
-    info.firstTitle = Localized.views
-    info.secondTitle = Localized.favorits
-    info.thirdTitle = Localized.codeObject
-    info.translatesAutoresizingMaskIntoConstraints = false
-    return info
-  }()
-  
+  // MARK: - Life
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = AppTheme.current().colors.container
-    
-    setupScreen()
-    addSubviewInView()
+
+    createUI()
     configureUI()
+    layout()
   }
   
   // MARK: - Functions
   
-  private func setupScreen() {
+  private func createUI() {
+    let subviews: [UIView] = [
+      container,
+      previewItemView,
+      addressItemView,
+      infoItemView
+    ]
     
+    subviews.forEach {
+      $0.translatesAutoresizingMaskIntoConstraints = false
+    }
+ 
+    container.addSubview(previewItemView)
+    container.addSubview(addressItemView)
+    container.addSubview(infoItemView)
+    
+    view.addSubview(container)
+  }
+  
+  private func configureUI() {
     // view
     view.backgroundColor = AppTheme.current().colors.screen
     
     // navigation
     navigationItem.title = "Title"
     
-    // imageView
-    image.layer.cornerRadius = 8
-    image.clipsToBounds = true
+    // container
+    container.backgroundColor = AppTheme.current().colors.container
     
-    // setupUI
-    currentPrice.setStyles(
-      UILabel.Styles.title3
-    )
+    // previewItemView
+    previewItemView.address = Localized.address
+    previewItemView.currentPrice = Localized.currentPrice
+    previewItemView.price = Localized.price
+    previewItemView.photo = UIImage(named: "photo")
     
-    price.setStyles(
-      UILabel.Styles.tiny,
-      UILabel.Styles.strikeThrough,
-      UILabel.ColorStyle.placeholders
-    )
+    // addressItemView
+    addressItemView.title = Localized.mainTitle
     
-    address.setStyles(
-      UILabel.Styles.tiny,
-      UILabel.Styles.secondary
-    )
-    
-    mainTitle.setStyles(
-      UILabel.Styles.tiny,
-      UILabel.ColorStyle.primary
-    )
-    
+    // infoItemView
+    infoItemView.firstTitle = Localized.views
+    infoItemView.secondTitle = Localized.favorits
+    infoItemView.thirdTitle = Localized.codeObject
   }
   
-  private func addSubviewInView() {
-    view.addSubview(container)
-    
-    container.addSubview(topView)
-    topView.addSubview(topViewStack)
-    topViewStack.addArrangedSubview(currentPrice)
-    topViewStack.addArrangedSubview(priceStack)
-    priceStack.addArrangedSubview(priceImage)
-    priceStack.addArrangedSubview(price)
-    topView.addSubview(address)
-    
-    container.addSubview(mainView)
-    mainView.addSubview(image)
-    mainView.addSubview(favoriteButton)
-    
-    container.addSubview(bottomView)
-    bottomView.addSubview(titleStack)
-    titleStack.addArrangedSubview(mainTitle)
-    titleStack.addArrangedSubview(phoneButton)
-    
-    bottomView.addSubview(infoStack)
-  }
-  
-  private func configureUI() {
-    
-    // setup
-    priceStack.alignment = .center
-    priceStack.spacing = 3
-    
-    titleStack.spacing = 60
-    titleStack.alignment = .center
-    
+  private func layout() {
     let margins = view.layoutMarginsGuide
-    
-    // topView
-    
-    container.topAnchor.constraint(equalTo: margins.topAnchor, constant: 10).isActive = true
-    container.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-    container.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-    
-    topView.topAnchor.constraint(equalTo: container.topAnchor).isActive = true
-    topView.leadingAnchor.constraint(equalTo: container.leadingAnchor).isActive = true
-    topView.trailingAnchor.constraint(equalTo: container.trailingAnchor).isActive = true
-    
-    topViewStack.topAnchor.constraint(equalTo: topView.topAnchor, constant: 12).isActive = true
-    topViewStack.leadingAnchor.constraint(equalTo: topView.leadingAnchor, constant: 16).isActive = true
-    topViewStack.trailingAnchor.constraint(equalTo: topView.trailingAnchor, constant: -16).isActive = true
-    
-    priceImage.widthAnchor.constraint(equalToConstant: 10).isActive = true
-    priceImage.heightAnchor.constraint(equalToConstant: 10.5).isActive = true
-    
-    address.topAnchor.constraint(equalTo: topViewStack.bottomAnchor, constant: 2).isActive = true
-    address.leadingAnchor.constraint(equalTo: topView.leadingAnchor, constant: 16).isActive = true
-    address.trailingAnchor.constraint(equalTo: topView.trailingAnchor, constant: -16).isActive = true
-    address.bottomAnchor.constraint(equalTo: topView.bottomAnchor, constant: -8).isActive = true
-    
-    // mainView
-    
-    mainView.topAnchor.constraint(equalTo: topView.bottomAnchor).isActive = true
-    mainView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-    mainView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-    
-    image.topAnchor.constraint(equalTo: mainView.topAnchor).isActive = true
-    image.leadingAnchor.constraint(equalTo: mainView.leadingAnchor, constant: 16).isActive = true
-    image.trailingAnchor.constraint(equalTo: mainView.trailingAnchor, constant: -16).isActive = true
-    image.bottomAnchor.constraint(equalTo: mainView.bottomAnchor).isActive = true
-    image.heightAnchor.constraint(equalToConstant: 207).isActive = true
-    
-    favoriteButton.topAnchor.constraint(equalTo: mainView.topAnchor, constant: 13).isActive = true
-    favoriteButton.trailingAnchor.constraint(equalTo: mainView.trailingAnchor, constant: -28.5).isActive = true
-    favoriteButton.heightAnchor.constraint(equalToConstant: 23).isActive = true
-    favoriteButton.widthAnchor.constraint(equalToConstant: 21.95).isActive = true
-    
-    // bottomView
-    
-    bottomView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-    bottomView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-    bottomView.topAnchor.constraint(equalTo: mainView.bottomAnchor).isActive = true
-    bottomView.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: 0).isActive = true
-    
-    titleStack.topAnchor.constraint(equalTo: bottomView.topAnchor, constant: 8).isActive = true
-    titleStack.leadingAnchor.constraint(equalTo: bottomView.leadingAnchor, constant: 16).isActive = true
-    titleStack.trailingAnchor.constraint(equalTo: bottomView.trailingAnchor, constant: -16).isActive = true
-    
-    mainTitle.bottomAnchor.constraint(lessThanOrEqualTo: titleStack.bottomAnchor, constant: -4).isActive = true
-    
-    phoneButton.widthAnchor.constraint(equalToConstant: 24).isActive = true
-    phoneButton.heightAnchor.constraint(equalToConstant: 24).isActive = true
-    
-    infoStack.topAnchor.constraint(equalTo: titleStack.bottomAnchor, constant: 4).isActive = true
-    infoStack.leadingAnchor.constraint(equalTo: bottomView.leadingAnchor, constant: 16).isActive = true
-    infoStack.trailingAnchor.constraint(equalTo: bottomView.trailingAnchor, constant: -16).isActive = true
-    infoStack.bottomAnchor.constraint(equalTo: bottomView.bottomAnchor, constant: -8).isActive = true
+    activeConstraints = [
+      container.topAnchor.constraint(equalTo: margins.topAnchor, constant: 10),
+      container.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      container.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      
+      previewItemView.topAnchor.constraint(equalTo: container.topAnchor),
+      previewItemView.leadingAnchor.constraint(equalTo: container.leadingAnchor),
+      previewItemView.trailingAnchor.constraint(equalTo: container.trailingAnchor),
+      
+      addressItemView.topAnchor.constraint(equalTo: previewItemView.bottomAnchor, constant: 8),
+      addressItemView.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 16),
+      addressItemView.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: -16),
+      
+      infoItemView.topAnchor.constraint(equalTo: addressItemView.bottomAnchor, constant: 4),
+      infoItemView.leadingAnchor.constraint(equalTo: container.leadingAnchor, constant: 16),
+      infoItemView.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: -16),
+      infoItemView.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: -8)
+    ]
+    NSLayoutConstraint.activate(activeConstraints)
   }
   
 }
