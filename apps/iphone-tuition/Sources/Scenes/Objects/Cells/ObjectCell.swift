f079d7c77dbde7d6b3e7520a44df0e17c91251d8
skyworkerxsky <pwuser@yandex.ru>
2020-05-08 01:42:48 +0500

    перенес в ячейки

---
+++ apps/iphone-tuition/Sources/Scenes/Objects/Cells/ObjectCell.swift
@@ -0,0 +1,8 @@
+//
+//  ObjectCell.swift
+//  AppLibrary
+//
+//  Created by Алексей Макаров on 08.05.2020.
+//
+
+import Foundation
---
+++ apps/iphone-tuition/Sources/Scenes/Objects/ViewModels/ObjectsViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ObjectsViewModel.swift
+//  AppLibrary
+//
+//  Created by Алексей Макаров on 08.05.2020.
+//
+
+import Foundation
