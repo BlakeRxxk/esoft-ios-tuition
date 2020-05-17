577f5c69c2a253e40c919b1b9a0641644157b46c
Blake Rxxk <blake.rxxk@gmail.com>
2020-05-17 12:32:42 +0500

    autogenerate file list for needle di

--- tools/scripts/generate_di_graph.sh
+++ tools/scripts/generate_di_graph.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
+OUTPUT=../../tools/feature_list.txt
+
+if [ ! -f "$OUTPUT" ]; then
+    echo "$OUTPUT does not exist"
+    echo "creating new one"
+   	touch $OUTPUT
+else
+	cp /dev/null $OUTPUT
+fi
+
+FEATURES_PATH=../../features
+INTEGRATIONS_PATH=../../integrations
+
+FEATURES_LIST=( $(find $FEATURES_PATH -name \*Sources -type d) )
+INTEGRATIONS_LIST=( $(find $INTEGRATIONS_PATH -name \*Sources -type d) )
+
+for feature in "${FEATURES_LIST[@]}"
+do
+   echo "${feature}/" >> $OUTPUT
+done
+
+for integration in "${INTEGRATIONS_LIST[@]}"
+do
+   echo "${integration}/" >> $OUTPUT
+done
+
 export SOURCEKIT_LOGGING=0 && \
 	../../tools/needle generate Sources/Generated/NeedleGenerated.swift Sources/ ../../tools/feature_list.txt \
 	--pluginized \
--- tools/scripts/generate_feature_list.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-OUTPUT=/tools/feature_list.txt
