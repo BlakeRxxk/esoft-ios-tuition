12ebe802cd155b94c5c5cda1a9d442163c182f1c
skyworkerxsky <pwuser@yandex.ru>
2020-05-26 18:56:48 +0500

    add new library

---
+++ features/Objects/ObjectsCore/Sources/ObjectsCore.swift
@@ -0,0 +1,11 @@
+//
+//  ObjectsCore.swift
+//  ObjectsCore
+//
+//  Created by Алексей Макаров on 26.05.2020.
+//  Copyright © 2020 Алексей Макаров. All rights reserved.
+//
+
+class ObjectsCore {
+
+}
