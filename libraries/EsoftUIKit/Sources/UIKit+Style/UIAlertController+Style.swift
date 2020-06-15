cc8eccd7e15f5c4ce3606986148202d299bb8c0d
Blake <blake.rxxk@gmail.com>
2020-06-15 14:34:31 +0500

    Create UIAlertController+Style.swift (#53)

---
+++ libraries/EsoftUIKit/Sources/UIKit+Style/UIAlertController+Style.swift
@@ -0,0 +1,27 @@
+//
+//  UIAlertController+Style.swift
+//  EsoftUIKit
+//
+//  Copyright © 2020 E-SOFT, OOO. All rights reserved.
+//
+
+import UIKit
+import Atlas
+import ThemeManager
+
+extension UIAlertController {
+  public enum Styles {
+    public static let `default` = Atlas.Style(name: "client.uialert.default") { (view: UIAlertController) in
+      let titleFont = [NSAttributedString.Key.font: ThemeManager.current().fonts.regularSemibold]
+      let messageFont = [NSAttributedString.Key.font: ThemeManager.current().fonts.tiny]
+
+      let titAttrString = NSMutableAttributedString(string: view.title ?? "", attributes: titleFont)
+      let msgAttrString = NSMutableAttributedString(string: view.message ?? "", attributes: messageFont)
+      
+      view.setValue(titAttrString, forKey: "attributedTitle")
+      view.setValue(msgAttrString, forKey: "attributedMessage")
+      view.view.subviews.first?.subviews.first?.subviews.first?.backgroundColor = ThemeManager.current().colors.container
+      view.view.tintColor = ThemeManager.current().colors.primary500
+    }
+  }
+}
